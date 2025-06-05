31bad395572c12fa4738d0cb49ab65b91921a780
St-St-T <stst09stanislav@yandex.ru>
2025-06-05 09:24:18 +0700

    Создание обработчика

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,19 +1,37 @@
 <?php
 
-AddEventHandler("iblock", "OnAfterIBlockElementUpdate", array("MyClass", "OnAfterIBlockElementUpdateHandler"));
+AddEventHandler("iblock", "OnAfterIBlockElementUpdate", Array("MyClass", "OnAfterIBlockElementUpdateHandler"));
+AddEventHandler("iblock", "OnAfterIBlockElementAdd", Array("MyClass", "OnAfterIBlockElementUpdateHandler"));
+
 class MyClass
 {
-    public static function OnAfterIBlockElementUpdateHandler(&$arFields)
-    {
-        if ($arFields["RESULT"]) {
+	public static function OnAfterIBlockElementUpdateHandler(&$arFields)
+	{
 
-            if ($arFields['IBLOCK_ID'] == 1) {
-                foreach ($arFields as $i) {
-                    AddMessage2Log($i . "<br>");
+		$iblockId = $arFields['IBLOCK_ID'];
+		$elementId = $arFields['ID'];
+
+
+		if ($iblockId == 1) {
+            $props = CIBlockElement::GetProperty($iblockId, $elementId, array(), array('CODE' => 'MAIN'))->Fetch();
+
+            if ($props && $props['VALUE'] == 'Y') {
+                $res = CIBlockElement::GetList(
+                    array(),
+                    array(
+                        'IBLOCK_ID' => $iblockId,
+                        '!ID' => $elementId,
+                        'PROPERTY_MAIN_VALUE' => 'Y'
+                    ),
+                    false,
+                    false,
+                    array('ID')
+                );
+                while ($item = $res->Fetch()) {
+                    CIBlockElement::SetPropertyValues($item['ID'], $iblockId, null, 'MAIN');
                 }
-            }
-        } else {
-            AddMessage2Log("Ошибка");
-        }
-    }
+			}
+		}
+	}
 }
+?>
